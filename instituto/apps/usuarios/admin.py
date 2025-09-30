4be32de65c899a8233c04ff6edabd3b599bef104
Denguesito <denguesito2013@gmail.com>
2025-09-29 21:26:58 -0300

    migracion del blog de la tecnicatura a bootstrap

---
+++ instituto/apps/usuarios/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Usuario
+
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    model = Usuario
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
---
+++ instituto/apps/usuarios/views.py
@@ -0,0 +1,109 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import TemplateView, UpdateView
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .forms import RegistroUsuarioForm, PerfilUsuarioForm
+from .models import Usuario
+
+# Registro
+class RegistroUsuarioView(View):
+    form_class = RegistroUsuarioForm
+    template_name = 'usuarios/registro.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            usuario = form.save()
+            login(request, usuario)
+            messages.success(request, f"Bienvenido {usuario.username}, tu cuenta fue creada correctamente.")
+            return redirect('index')
+        return render(request, self.template_name, {'form': form})
+
+# Login
+class LoginUsuarioView(View):
+    template_name = 'usuarios/login.html'
+
+    def personalizar_formulario(self, form):
+        """Agrega clases y atributos Bootstrap a los campos del formulario"""
+        form.fields['username'].label = 'Usuario'
+        form.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Ingrese su usuario'
+        })
+        form.fields['password'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Ingrese su contraseña'
+        })
+
+    def get(self, request):
+        form = AuthenticationForm()
+        self.personalizar_formulario(form)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = AuthenticationForm(data=request.POST)
+        self.personalizar_formulario(form)
+
+        if form.is_valid():
+            usuario = form.get_user()
+            login(request, usuario)
+            messages.success(request, f"Bienvenido {usuario.username}")
+            return redirect('index')
+        else:
+            messages.error(request, "Usuario o contraseña incorrectos")
+
+        return render(request, self.template_name, {'form': form})
+
+# Perfil detalle
+class PerfilDetalleView(LoginRequiredMixin, TemplateView):
+    template_name = 'usuarios/perfil.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['usuario'] = self.request.user
+        return context
+
+# Perfil editar
+class PerfilUsuarioView(LoginRequiredMixin, UpdateView):
+    model = Usuario
+    form_class = PerfilUsuarioForm
+    template_name = 'usuarios/editar_perfil.html'
+    success_url = reverse_lazy('usuarios:perfil')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def form_valid(self, form):
+        messages.success(self.request, "Perfil actualizado correctamente")
+        return super().form_valid(form)
+    
+
+class EliminarUsuarioView(LoginRequiredMixin, View):
+    def get(self, request):
+        return render(request, 'usuarios/eliminar_perfil.html')
+
+    def post(self, request):
+        usuario = request.user
+        logout(request)
+        usuario.delete()
+        messages.success(request, "Tu cuenta fue eliminada correctamente.")
+        return redirect('index')
+
+
+# Logout con confirmación
+class LogoutUsuarioView(LoginRequiredMixin, TemplateView):
+    template_name = 'usuarios/logout.html'
+
+    def post(self, request):
+        logout(request)
+        messages.success(request, "Has cerrado sesión correctamente")
+        return redirect('index')
